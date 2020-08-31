2b2a5c25b4a31b299c4c1f50eaf0db5c337f0649
lihua <poinseting@163.com>
2020-08-31 21:00:43 +0800

    localPosition证明

---
+++ Assets/Pivot/PivotPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PivotPosition : MonoBehaviour
+{
+	public RectTransform pivot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		pivot.localPosition = Vector3.zero;
+
+	}
+}
---
+++ Assets/Pivot/SamePosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamePosition : MonoBehaviour
+{
+	public RectTransform otherUI;
+	public Transform otherCube;
+	public RectTransform self;
+
+    // Update is called once per frame
+    void Update()
+    {
+		otherUI.position = self.position;
+		otherCube.position = self.position;
+	}
+}
